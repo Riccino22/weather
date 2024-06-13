f3eb4a03dcfae398d148ff25bc3cc75d91f2b191
riccino <riccardorinojosam@gmail.com>
2024-06-13 18:14:32 -0300

    structuring the data

--- data.py
+++ data.py
@@ -3,9 +3,10 @@ import os
 from dotenv import load_dotenv
 
 load_dotenv()
-def get(city, ):
+def get(city, days):
     response = requests.get(f"http://api.openweathermap.org/data/2.5/forecast?q={city}&appid={os.environ.get("API_OPENWEATHER")}")
-    return response.json()
+    data = response.json()
+    return data["list"][1]
 
 ""
-    
+print(get("montevideo", 3))
